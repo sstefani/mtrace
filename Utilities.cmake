4c5f0c20b484db4da63e9cbf3f73c1a264a71ce3
Stefani Seibold <stefani@seibold.net>
2017-12-19 11:18:31 +0100

    fix echo_all_cmake_variable_values function
    clean up the output

--- Utilities.cmake
+++ Utilities.cmake
@@ -123,11 +123,9 @@ endfunction()
 
 
 function(echo_all_cmake_variable_values)
-  message(STATUS “”)
   get_cmake_property(vs VARIABLES)
   foreach(v ${vs})
-    message(STATUS “${v}=’${${v}}'”)
+    message(STATUS "${v}='${${v}}'")
   endforeach(v)
-  message(STATUS “”)
 endfunction()
 
